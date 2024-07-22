aef937e93ffeef74aafef4d454536be165e815f1
Briggette Roman <briggette.roman@gmail.com>
2024-07-22 17:00:41 -0300

    Estruturação basica do uma função e as rotas(não é melhor forma)

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # Web Hacking na pratica
+Desenvolvimentos de projetos iniciais para analise de segurança
+## Sumario
+* Introdução
+* Programação em PHP
+* 
--- programming/proj01/index.php
+++ programming/proj01/index.php
@@ -1,5 +1,26 @@
 <?php
     require_once 'autoload.php';
 
-    echo " oii Live";
+
+    // A pior forma de fazer um route,  então qual é uma melhor forma?
+    $route = $_SERVER['REQUEST_URI'];
+
+    switch ($route) {
+        case '/':
+            $auth = new AuthController();
+            if ($auth -> checkAuth()) {
+                echo 'Olá, voce esta autenticado!.';
+            } else {
+                echo 'Olá, voce NÃO esta autenticado!.';
+            }
+            break;
+
+        case '/login':
+            $controller = new AuthController();
+            break;
+
+        default:
+            echo "Rota não encontrada!";
+            break;
+    }
 ?>
